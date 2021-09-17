be13548ef77f064d286021c7d05b8352d8a9d04f
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2021-09-17 11:40:52 +0200

    Accessing database through repository

--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -1,5 +1,7 @@
 using DevIO.App.Data;
+using DevIO.Business.Interfaces;
 using DevIO.Data.Context;
+using DevIO.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -41,7 +43,10 @@ namespace DevIO.App
             services.AddRazorPages();
 
             services.AddScoped<MeuDbContext>();
-            
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
